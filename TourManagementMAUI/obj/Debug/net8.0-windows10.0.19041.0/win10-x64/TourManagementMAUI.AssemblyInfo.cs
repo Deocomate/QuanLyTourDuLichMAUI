32c64e964325c6b5e7fdaeb54152977cca3ea87e
Deocomate <deocomate@GMAIL.COM>
2024-03-30 08:18:30 +0700

    Ignore

--- TourManagementMAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TourManagementMAUI.AssemblyInfo.cs
+++ TourManagementMAUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/TourManagementMAUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourManagementMAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+913dad238ee4af0998be78b86353367874abadd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9d279954b3719f6140c865511c26e83a41795bb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourManagementMAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourManagementMAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
